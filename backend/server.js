5fbefedb5f0386bb7cfe0972235bd27a6ac431c1
Tushar Goyal <tushargoyalq@gmail.com>
2026-04-15 22:52:40 +0530

    fix port issue

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ app.get("/", (req, res) => {
 });
 
 // Server start
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
